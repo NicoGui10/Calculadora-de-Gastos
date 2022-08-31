b08979acfc03254fc2e564e3d16671cc436ce7e5
NicoGuido10 <nicolasguido10@gmail.com>
2022-08-31 16:51:46 -0300

    inicio calculadora de vacaciones

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let vocationCalc = document.getElementById("vacationCal");
+
+vocationCalc.addEventListener("submit", calcExpenses);
+
+function getValue() {
+    let destiny = document.getElementById("destiny").value,
+        budget = document.getElementById("budget").value,
+        acomodation = document.getElementById("acomodation").value,
+        food = document.getElementById("food").value,
+        pass =document.getElementById("pass").value,
+        excursions=document.getElementById("excursions").value;
+
+    return { destiny, budget, acomodation,food, pass, excursions }
+
+}
+
+function calcExpenses(e) {
+    e.preventDefault();
+
+    const { destiny, budget, acomodation, food, pass, excursions } = getValue();
+
+    let Expenses = parseInt(acomodation) + parseInt(food)+ parseInt(pass)+ parseInt(excursions);
+    let balance = budget - Expenses;
+
+    SM(budget, balance, destiny)
+
+    /*console.log(destiny,budget,acomodation,food, pass, excursions)*/
+}
+
+function SM(budget, balance, destiny) {
+    let result = document.getElementById("result");
+    let dataprint = document.createElement("div")
+    dataprint.innerHTML = `
+        <div class = "container-data">
+      
+            <div class="data__print">
+                <h3>${destiny}</h3>
+            </div>
+
+            <div class="data__print">
+                <h3>${budget} $</h3>
+            </div>
+
+            <div class="data__print">
+                <h3>${balance} $</h3>
+            </div>
+        </div>
+        `   
+    result.appendChild(dataprint)
+
+    reset();
+}
+
+function reset() {
+    document.getElementById("vacationCal").reset()
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Calculadora De Presupuesto</h1>
+<p>Corresponde al primer desafio obligatorio, la idea es una calculadora de gastos para tus vacaciones</p>
